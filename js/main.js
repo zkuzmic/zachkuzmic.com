bc7adf2d4ebc1c8d9d7578d01c090d97414cab0c
Zach Kuzmic <zachkuzmic@gmail.com>
2016-01-08 15:48:08 -0600

    home page finito

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function() {
 	var nav = responsiveNav(".main-nav", {
 		insert: "after",
 		openPos: "static",
-		label: "<img src='http://zachkuzmic.com/img/menu.png' alt='Menu'>"
+		label: "<img src='/img/menu.png' alt='Menu'>"
 	});
 
 });
