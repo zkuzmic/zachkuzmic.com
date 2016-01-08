1d07d9fc9510d9e142581dd3395a95e4f6076130
Zach Kuzmic <zachkuzmic@gmail.com>
2016-01-08 14:03:17 -0600

    header styling and mobile nav functionality completed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+
+	var nav = responsiveNav(".main-nav", {
+		insert: "after",
+		openPos: "static",
+		label: "<img src='http://zachkuzmic.com/img/menu.png' alt='Menu'>"
+	});
+
+});
